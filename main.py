30e0eb5e2183518a04ad61e4998e7f6a82774f25
tristantyler <52974257+tristantyler@users.noreply.github.com>
2019-10-18 12:42:48 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import time
 import math
 import matplotlib.pyplot as plt
+import pickle
+import os
 
 
 class Node:
@@ -23,11 +25,12 @@ def graphSetup():
 
 def graph(ls):
     """Takes the x and y values from nodes and makes pairs and graphs them. Takes a node list."""
+    ls = convertToNodes(ls)
     graphSetup()
     xlist = []
     ylist = []
 
-    for node in ls[1:]:
+    for node in ls:
         xlist.append(node.x)
         ylist.append(node.y)
 
@@ -35,55 +38,56 @@ def graph(ls):
     plt.show()
 
 
-def convertToList(data):
+def convertToNodes(data):
     """Takes a dataframe object and converts them to a list of nodes with values and their x,y coords
     on a 28x28 image"""
     nodelist = []
 
-    for row in range(len(data)):
-        dt = list(data.iloc[row])
-        dtl = [dt[0]]
-        for i, node in enumerate(dt[1:]):
-            if int(node) > 1:
-                x = (i % 28)
-                y = int(i / 28)
-                dtl.append(Node(x, y, int(node)))
-        nodelist.append(dtl)
+    for i, node in enumerate(data):
+        if int(node) > 1:
+            x = (i % 28)
+            y = int(i / 28)
+            nodelist.append(Node(x, y, int(node)))
     return nodelist
 
 
 def convertToList2(data):
-    nodelist = []
-    for row in range(len(data)):
-        dt = list(data.iloc[row])
-        nodelist.append(dt)
-    return nodelist
+    return [list(data.iloc[row]) for row in range(len(data))]
 
 
-def calculateDistance(testrow, traindata, k):
+def convertToList5(data, traintrue=False):
+    betterlist = []
+    nl = [list(data.iloc[row]) for row in range(len(data))]
+    for row in nl:
+        if traintrue:
+            betterrow = [row[0]]
+        else:
+            betterrow = []
+        for count, node in enumerate(row):
+            if node != 0:
+                betterrow.append(count)
+        betterlist.append(betterrow)
+
+    return betterlist
+
+
+def calculateDistance2(testrow, traindata, k):
     distanceList = []  # holds the distance calculation for each train data row
     indlist = []  # holds indexes for the k number of closest train data rows
     dlist = []  # holds the distance value for the k number of closest train data rows
-    tot = 0  # sums of the k number of closest distance values
     endlist = [0 for x in range(10)]  # holds total weighted votes for each class based on index in list
 
     for td in traindata:  # calculates distance for each train data row from testrow
-        total = 0
-        for i in range(1, len(testrow)):
-            if testrow[i] == 0 and td[i] == 0:
-                pass
-            elif testrow[i] == 0:
-                total += 1
-            elif td[i] == 0:
-                total += 1
-        distanceList.append(1 / (math.sqrt(total)))
+        a = len(set(testrow).intersection(td))
+        b = abs(len(testrow) - a) + abs(len(td) - a)
+        distanceList.append(1 / b)
 
     for i in range(k):  # gets the k number of closest train data rows
         ind = distanceList.index(max(distanceList))
         indlist.append(ind)
         dlist.append(max(distanceList))
-        tot += max(distanceList)
-        distanceList.pop(ind)
+        distanceList[ind] = 0
+    tot = sum(dlist)
 
     for i in range(k):  # assigns votes to respective classes
         endlist[traindata[indlist[i]][0]] += dlist[i] / tot
@@ -91,21 +95,55 @@ def calculateDistance(testrow, traindata, k):
     return endlist.index(max(endlist))  # returns the value of the max in list, i.e the class
 
 
+def prepareData(skip=False):
+    if skip:
+        print("Converting Train Data to List...")
+        trainnodelist = convertToList5(pd.read_csv("MNIST_train.csv"), True)
+        print("Converting Test Data to List...")
+        testnodelist = convertToList5(pd.read_csv("MNIST_test.csv"), True)
+
+        return trainnodelist, testnodelist
+
+    if not os.path.exists("knn.pckl"):
+        print("Making Pickle...")
+        print("Converting Correct Data to List...")
+        corlist = convertToList2(pd.read_csv("submission.csv"))
+        print("Converting Train Data to List...")
+        trainnodelist = convertToList5(pd.read_csv("train.csv"), True)
+        print("Converting Test Data to List...")
+        testnodelist = convertToList5(pd.read_csv("test.csv"))
+
+        with open("knn.pckl", "wb") as file:
+            pickle.dump((corlist, trainnodelist, testnodelist), file)
+    else:
+        print("Getting Pickle..")
+        with open("knn.pckl", "rb")as file:
+            corlist, trainnodelist, testnodelist = pickle.load(file)
+
+    return corlist, trainnodelist, testnodelist
+
+
 def main():
     start = time.time()
-    traindata = pd.read_csv("MNIST_train.csv")
-    testdata = pd.read_csv("MNIST_test.csv")
-
-    trainnodelist = convertToList2(traindata)
-    testnodelist = convertToList2(testdata)
+    #corlist, trainnodelist, testnodelist = prepareData()
+    trainnodelist, testnodelist = prepareData(True)
 
     count = 0
-    for i in testnodelist:
-        calcvalue = calculateDistance(i, trainnodelist, 7)
-        print("Desired class:", i[0], "Computed class:", calcvalue)
-        if i[0] == calcvalue:
+    #print("Took ", time.time() - start, "seconds")
+    start = time.time()
+    print("Computing classes...")
+    for j, i in enumerate(testnodelist):
+        startcomp = time.time()
+
+        calcvalue = calculateDistance2(i, trainnodelist, 9)
+        #expected = corlist[j][1]
+        expected = i[0]
+        print("Desired class:", expected, "Computed class:", calcvalue, "| Took", round((time.time() - startcomp), 4),
+              "seconds")
+        if expected == calcvalue:
             count += 1
-    print("{0}%".format((count / len(testnodelist)) * 100))
+        print("{0}%".format(round((count / (j + 1) * 100), 4)), j + 1)
+    print("{0}%".format(round((count / len(testnodelist)) * 100), 4))
 
     print("Took ", time.time() - start, "seconds")
 
